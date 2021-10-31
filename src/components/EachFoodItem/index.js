07bfc5b0b7b71debb60aa9cdd9c84463e465c5b4
tejajavvaji <javvajikumar123@gmail.com>
2021-10-31 04:35:56 +0000

    Local Storage Started

--- src/components/EachFoodItem/index.js
+++ src/components/EachFoodItem/index.js
@@ -14,8 +14,44 @@ class EachFoodItem extends Component {
   //   }
 
   onIncrement = sendIncrement => {
-    sendIncrement()
-    this.setState(prevState => ({quantity: prevState.quantity + 1}))
+    // sendIncrement()
+    const {quantity} = this.state
+    // this.setState(prevState => ({quantity: prevState.quantity + 1}))
+    this.setState({quantity: quantity + 1}, this.quantityIncreaseLocalStorage)
+  }
+
+  quantityIncreaseLocalStorage = () => {
+    const localData = JSON.parse(localStorage.getItem('CartData'))
+    const {data} = this.props
+    const updatedData = localData.map(eachItem => {
+      if (eachItem.id === data.id) {
+        return {
+          cost: eachItem.cost,
+          food_type: eachItem.food_type,
+          id: eachItem.id,
+          image_url: eachItem.image_url,
+          name: eachItem.name,
+          quantity: eachItem.quantity,
+          rating: eachItem.rating,
+        }
+      }
+      return eachItem
+    })
+    localStorage.setItem('CartData', JSON.stringify(updatedData))
+  }
+
+  addItemToLocalStorage = () => {
+    const localData = JSON.parse(localStorage.getItem('CartData'))
+
+    const {data} = this.props
+    console.log(data)
+    const {quantity} = this.state
+    if (localData === null) {
+      localStorage.setItem('CartData', JSON.stringify([{...data, quantity}]))
+    } else {
+      localData.push({...data, quantity})
+      localStorage.setItem('CartData', JSON.stringify(localData))
+    }
   }
 
   onDecrement = (sendDecrement, sendDelete) => {
@@ -96,11 +132,16 @@ class EachFoodItem extends Component {
 
           /* this.checkCartData(cartData, id) */
 
-          const {showCounter} = this.state
+          const {showCounter, quantity} = this.state
+          console.log(quantity)
 
           const addItem = () => {
-            this.setState({showCounter: true, quantity: 1})
-            addProductToCart({...data, quantity: 1})
+            this.setState(
+              {showCounter: true, quantity: 1},
+              this.addItemToLocalStorage,
+            )
+
+            /* addProductToCart({...data, quantity: 1}) */
           }
 
           const sendDelete = () => {
